aee447129ee54c08c209df1a620874ca93f38305
Tomasz Martyniak <tom4r@users.noreply.github.com>
2018-09-10 21:10:50 +0200

    Added checking if documentation contains polish letters.

---
+++ DocSaw/Rules/DocumentationMustBeInEnglish.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using DocSaw.Confluence;
+using Newtonsoft.Json;
+
+namespace DocSaw.Rules
+{
+    public class DocumentationMustBeInEnglish : IRule
+    {
+        public void Check(Page page, ErrorReporter reporter)
+        {
+            if (HasPolishLetters(page.Title))
+            {
+                reporter.Report(page, "Page title is in Polish");
+            }
+
+            if (DoesBodyContainPolishLettersInLowercaseWords(page.Body.AtlasDocFormat.Value))
+            {
+                reporter.Report(page, "Page has Polish letters");
+            }
+        }
+
+        private bool HasPolishLetters(string text)
+        {
+            var arePolishLetters = text.ToLower().IndexOfAny(new char[] { 'ą', 'ę', 'ć', 'ó', 'ś', 'ł', 'ż', 'ź', 'ń' }) != -1;
+            return arePolishLetters;
+        }
+
+        private bool DoesBodyContainPolishLettersInLowercaseWords(string text)
+        {
+            var terms = text.Trim().Split(' ');
+
+            return terms.Where(x => x == x.ToLower()).Any(HasPolishLetters);
+        }
+    }
+}
