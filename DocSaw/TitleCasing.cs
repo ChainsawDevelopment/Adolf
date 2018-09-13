e20898a5a38f3f1b85a8067c4037e352105148be
Maciej Nowak <maciejt.nowak@gmail.com>
2018-09-13 20:47:14 +0200

    Tests for camel case detector

--- DocSaw/TitleCasing.cs
+++ DocSaw/TitleCasing.cs
@@ -1,63 +1,152 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Text;
+using Microsoft.Extensions.Primitives;
 
 namespace DocSaw
 {
     public static class TitleCasing
     {
+        private static readonly string[] Articles = new[] {"a", "an", "the"};
+
+        private static readonly string[] Conjunction2 = new[]
+        {
+            "and", "but", "or", "for", "nor", "so", "yet"
+        };
+
+        private static readonly string[] Prepositions = new[]
+        {
+            "about", "above", "across", "after", "against", "along", "among", "around", "as", "at", "before", "behind", "below", "beneath", "beside", "between", "beyond", "by",
+            "down", "during", "except", "for", "from", "in", "inside", "into", "like", "near", "of", "off", "on", "onto", "out", "outside", "over", "past", "regarding", "since",
+            "through", "throughout", "till", "toward", "under", "until", "up", "upon", "with", "within", "without"
+        };
+
+        private static readonly string[] To = new[]
+        {
+            "to"
+        };
+
+        private static readonly char[] Separators = new[]
+        {
+            ' ', '-', ',', '.', '(', ')', '/'
+        };
+
+        private static readonly string[] Units = new[]
+        {
+            "km"
+        };
+
         public static bool IsCamelCase(string title)
         {
-            var terms = title.Trim().Split(' ');
+            string tmp;
+            return IsCamelCase(title, out tmp);
+        }
 
-            var allWordsStarsWithUpperLetter = terms
-                .Except(new[]
+        public static bool IsCamelCase(string title, out string invalidStart)
+        {
+            bool result = true;
+            invalidStart = "";
+
+            var remaining = new StringSegment(title).Trim();
+            var initialLength = remaining.Length;
+
+            if (remaining.Length > 0 && !char.IsLetterOrDigit(remaining[0]))
+            {
+                invalidStart = title;
+                return false;
+            }
+
+
+            var allowedLowerCase =
+                Articles
+                    .Union(Conjunction2)
+                    .Union(Prepositions)
+                    .Union(To)
+                    .Union(Units)
+                    .ToList();
+
+            while (remaining.Length > 0)
+            {
+                if (Separators.Contains(remaining[0]))
                 {
-                    "after",
-                    "although",
-                    "as",
-                    "as",
-                    "if",
-                    "as",
-                    "long",
-                    "as",
-                    "because",
-                    "before",
-                    "despite",
-                    "even",
-                    "if",
-                    "even",
-                    "though",
-                    "if",
-                    "in",
-                    "order",
-                    "that",
-                    "rather",
-                    "than",
-                    "since",
-                    "so",
-                    "that",
-                    "that",
-                    "though",
-                    "unless",
-                    "until",
-                    "when",
-                    "where",
-                    "whereas",
-                    "whether",
-                    "and",
-                    "while",
-                    "of",
-                    "the",
-                    "by",
-                    "-",
-                    "for",
-                    "on"
-                })
-                .Where(x => !String.IsNullOrWhiteSpace(x))
-                .Where(x => Char.IsLetter(x[0]))
-                .All(x => Char.IsUpper(x[0]));
+                    remaining = remaining.Subsegment(1);
+                    continue;
+                }
 
-            return allWordsStarsWithUpperLetter;
+                if (char.IsDigit(remaining[0]))
+                {
+                    remaining = SplitAtWhitespace(remaining);
+                    continue;
+                }
+
+                {
+                    var part = allowedLowerCase.FirstOrDefault(Predicate(remaining));
+
+                    if (part != null)
+                    {
+                        if (remaining.Length == initialLength)
+                        {
+                            part = char.ToUpper(part[0]) + part.Substring(1);
+                        }
+
+                        var matchingInTitle = remaining.Subsegment(0, part.Length);
+
+                        if (matchingInTitle != part)
+                        {
+                            invalidStart = remaining.ToString();
+                            return false;
+                        }
+
+                        remaining = remaining.Subsegment(part.Length).TrimStart();
+                        continue;
+                    }
+                }
+
+                if (!char.IsUpper(remaining[0]))
+                {
+                    invalidStart = remaining.ToString();
+                    return false;
+                }
+
+                remaining = SplitAtWhitespace(remaining);
+            }
+
+            return true;
+        }
+
+        private static StringSegment SplitAtWhitespace(StringSegment segment)
+        {
+            var idx = segment.IndexOf(' ');
+            if (idx == -1)
+            {
+                return StringSegment.Empty;
+            }
+
+            return segment.Subsegment(idx + 1).TrimStart();
+        }
+
+        private static Func<string, bool> Predicate(StringSegment remaining)
+        {
+            return x =>
+            {
+                if (!remaining.StartsWith(x, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    return false;
+                }
+
+                if (remaining.Length == x.Length)
+                {
+                    return true;
+                }
+
+                if (!char.IsWhiteSpace(remaining[x.Length]))
+                {
+                    return false;
+                }
+
+                return true;
+            };
         }
     }
 }
---
+++ Tests/CamelCaseTest.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Collections.Generic;
+using DocSaw;
+using Xunit;
+
+namespace Tests
+{
+    public class CamelCaseTest
+    {
+        [Theory]
+        [MemberData(nameof(CorrectTitles))]
+        public void CorrectTitle(string title)
+        {
+            string invalidStart;
+            var result = TitleCasing.IsCamelCase(title, out invalidStart);
+            Assert.True(result, $"Title '{title}' should be recognized as valid camel case (invalid: '{invalidStart}')");
+        }
+
+        [Theory]
+        [MemberData(nameof(IncorrectTitles))]
+        public void IncorrectTitle(string title)
+        {
+            var result = TitleCasing.IsCamelCase(title);
+            Assert.False(result, $"Title '{title}' should be recognized as invalid camel case");
+        }
+
+        public static IEnumerable<object[]> CorrectTitles()
+        {
+            Func<string, object[]> tc = s => new object[] {s};
+
+            yield return tc("A");
+            yield return tc("Single");
+            yield return tc("Two Words");
+            yield return tc("Drift Due to Initial Conditions");
+            yield return tc("Moving the Chicken");
+            yield return tc("Chicken Despite Chicken");
+            yield return tc("Chicken and Chicken");
+            yield return tc("Chicken after Chicken");
+            yield return tc("Customisation - Custom OBC Code, APIs, Interacting with Subsystems");
+            yield return tc("QM PLD Integration with a Satellite, Assembly Verification");
+            yield return tc("Satellite Is Transported for Integration with a P-POD, Integration with a P-POD");
+            yield return tc("4.2 Chickencraft Heritage and Specification");
+            yield return tc("Chicken Chicken Chicken (CCC)");
+            yield return tc("Chicken Chicken at This Chicken:");
+            yield return tc("   Chicken Chicken at This Chicken");
+            yield return tc("");
+
+            yield return tc("600 km");
+            yield return tc("UHF / VHF"); // will do for now
+            // yield return tc("UHF/VHF"); // would be better
+
+        }
+
+        public static IEnumerable<object[]> IncorrectTitles()
+        {
+            Func<string, object[]> tc = s => new object[] {s};
+
+            yield return tc("single");
+            yield return tc("two Words");
+            yield return tc("Two words");
+            yield return tc("Chicken After Chicken");
+            yield return tc("Simplified Approaches (the \x201cDose-depth\x201d Curve)");
+            yield return tc("Title with \"Quotation \" Mark");
+            yield return tc("Title with &");
+            yield return tc("Title with |");
+            yield return tc("Random & Acoustic Chickens");
+            yield return tc("Detailed 1-D, 2-D or Full 3-D Chicken Transport Calculations (Monte-Carlo & Finite Difference Models) (TODO)");
+            yield return tc("Handling and Transportation Loads - Quasi-static G-loads | Summary");
+            yield return tc("Chickement, Chickenbling + Cochickening, Chickenables Released");
+            yield return tc("- A-B Interface"); // non printable character
+            yield return tc("- Chicken");
+            //yield return tc("UHF / VHF"); // will do for now
+        }
+    }
+}
